3acee3b9876d6883b8a0e2f9acc6034ae9918378
Mark A Guza <guzamaguza@gmail.com>
2019-12-20 05:29:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //should return an array of nodes in the order they were visited
 function bfs(rootNode, vertices, edges){
   let discoveredList = [rootNode]; //this list keeps track of which vertices have been discovered (temporary array set up to iterate on)
-  let discoverOrder = [rootNode];  //this list keeps track of the order in order 
-  rootNode.distance = 0; //sets the rootNode distance to zero 
+  let discoverOrder = [rootNode];  //this list keeps track of the order in order
+  rootNode.distance = 0; //sets the rootNode distance to zero
   while (discoveredList.length != 0){
-    let currentNode = discoveredList.shift(); //removes first element and uses it in the current iteration 
+    let currentNode = discoveredList.shift(); //removes first element and uses it in the current iteration
 
-//find adjacent nodes 
-    let adjacentNodes = findAdjacentNodes(currentNode, vertices, edges); //finds adjacent nodes to current node 
+//find adjacent nodes
+    let adjacentNodes = findAdjacentNodes(currentNode, vertices, edges); //finds adjacent nodes to current node
     discoverOrder = discoverOrder.concat(adjacentNodes);  //adds adjacent node to discoverOrder to keep track of order found
 //process adjacent nodes and adds them to temporary iteration array (discoveredList)
     markDistanceAndPredecessor(currentNode, adjacentNodes);  //maps array of the adjacent nodes to the current node with this function
     dicoveredList = discoveredList.concat(adjacentNodes);  //adds adjacent nodes to the discovered list (once they've been mapped and processed)
   }
-  return discoverOrder 
+  return discoverOrder
 }
 
 //should return an array of adjacent nodes
@@ -22,13 +22,13 @@ function findAdjacentNodes(nodeName, vertices, edges){
   return edges.filter(function(edge){return edge.includes(nodeName)}) //first filters edges that have the nodeNames in them
               .map(function(edge){return edge.filter(function(node){return (node != nodeName)})[0]}) //maps edges where nodes are not equal in name
               .map(function(name){return findNode(name, vertices)}) //maps the nodes that match vertex names
-              .filter(function(node){return node.distance == null}) //filters all the ones that have null distances 
+              .filter(function(node){return node.distance == null}) //filters all the ones that have null distances
 }
 
 
 //should return an array of adjacent nodes
 function markDistanceAndPredecessor(predecessor, adjacentNodes){
-  adjacentNodes.map(function(node){  //nonmutating method 
+  adjacentNodes.map(function(node){  //nonmutating method
     node.distance = predecessor.distance + 1;
     node.predecessor = predecessor;
   })
@@ -38,7 +38,7 @@ function addVertexToQueue(node){
   return queue.push(node)
 }
 
-//returns vertices that have the given node name 
+//returns vertices that have the given node name
 function findNode(nodeName, vertices){
   return vertices.find(function(vertex){
     return vertex.name == nodeName;
